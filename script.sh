d7271eb6c7c89e82653737a852ef0d384e556920
arturrpereira <artur.fernando@protonmail.com>
2022-08-03 21:24:15 -0300

    Adicionando no script a instalação das dependências do i3 gaps

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install alsa-utils brightnessctl chromium cmatrix curl gimp i3blocks i3status make meson neofetch neovim network-manager sudo thunar tmux uzip xfce4-power-manager xfce4-terminal xinput xorg zsh -y
+sudo apt install dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
 
 #Conectar ao wifi pelo network manager
 #nmcli r wifi on
@@ -19,4 +20,3 @@ sudo apt install alsa-utils brightnessctl chromium cmatrix curl gimp i3blocks i3
 #meson ..
 #ninja 
 #sudo ninja install
-
