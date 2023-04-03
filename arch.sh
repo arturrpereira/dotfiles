6c5bdc0b83e4c3d756838c72185a4fa578eeabff
Artur Fernando <artur.fernando@protonmail.com>
2023-04-03 09:09:33 -0300

    Adding configs

---
+++ arch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm alacritty alsa-utils amd-ucode arandr bspwm chromium curl dbeaver discord dmenu emacs exa filezilla gnome-keyring neovim networkmanager nitrogen nodejs noto-fonts noto-fonts-emoji npm numlockx pavucontrol picom polybar postgresql pulseaudio rofi sxhkd syncthing xorg
