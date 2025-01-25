d1c7241e60316e9181fe00b53a81aef682ad8a86
Artur Fernando <artur.fernando@protonmail.com>
2025-01-25 14:15:09 -0300

    add more plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,7 +13,8 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme "gruvbox"
+vim.o.background = "dark"
 
 -- Reserve a space in the gutter
 vim.opt.signcolumn = 'yes'
